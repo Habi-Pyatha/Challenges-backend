73f34c049bddd3a00b37e7ba1ee764dc59353f30
HabiPyatha <unionhab@gmail.com>
2024-11-17 12:39:45 +0545

    user devise added

--- app/controllers/api/v1/challenges_controller.rb
+++ app/controllers/api/v1/challenges_controller.rb
@@ -1,21 +1,56 @@
 module Api
   module V1
     class ChallengesController < ApplicationController
+      before_action :set_challenge, only: [ :show, :update, :destroy ]
       def index
         @challenges=Challenge.all
         render json: @challenges
       end
 
       def create
+        challenge=Challenge.new(challenge_params)
+
+
+        if challenge.save
+          render json: { message: "Challenge added sucessfully", data: challenge }, status: :created
+
+        else
+          render json: { message: "Failed to add challenge.", errors: challenge.errors.full_messages }, status: :unprocessable_entity
+        end
       end
 
       def show
+        if @challenge
+          render json: { message: "Challenge found", data: @challenge }, status: :ok
+        else
+          render json: { message: "Challenge not found" }, status: :not_found
+        end
       end
 
       def update
+        if @challenge.update(challenge_params)
+          render json: { message: "Challenge updated", data: @challenge }, status: :ok
+        else
+          render json: { message: "Challenge update failed" }, status: :unprocessable_entity
+        end
       end
 
       def destroy
+        if @challenge.destroy
+          render json: { message: "Challenge deleted sucessfully" }, status: :ok
+        else
+          render json: { message: "Challenge destroy failed." }, status: :not_found
+        end
+      end
+
+      private
+      def challenge_params
+        params.require(:challenge).permit(:title, :description, :start_date, :end_date)
+      end
+
+      def set_challenge
+        @challenge=Challenge.find_by(id: params[:id])
+        render json: { message: "Challenge not found" }, status: :not_found unless @challenge
       end
     end
   end
